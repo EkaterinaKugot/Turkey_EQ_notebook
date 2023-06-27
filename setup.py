ca0485a7c7c3b71f14c30f9f1bbc9ace792f3281
Ekaterina_Kugot <kali.kugot@yandex.ru>
2023-06-27 20:39:54 +0800

    all at once

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+import json
+import os
+
+def read_pipenv_dependencies(fname):
+    """Получаем из Pipfile.lock зависимости по умолчанию."""
+    filepath = os.path.join(os.path.dirname(__file__), fname)
+    with open(filepath) as lockfile:
+        lockjson = json.load(lockfile)
+        return [dependency for dependency in lockjson.get('default')]
+
+if __name__ == '__main__':
+    setup(
+        name='turkey_eq',
+        version='1.0',
+        description='Package for practical work on Turkey_EQ.',
+        license='MIT',
+        url='https://github.com/EkaterinaKugot/Turkey_EQ_notebook.git',
+        packages=['turkey_eq'],       
+        install_requires=[
+              *read_pipenv_dependencies('Pipfile.lock'),
+        ],
+        python_requires='>=3',
+    )

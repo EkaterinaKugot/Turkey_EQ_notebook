031b8d2b757606d2f8ffd39225e0395d8a6a4327
Ekaterina_Kugot <kali.kugot@yandex.ru>
2023-06-29 19:52:02 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_pipenv_dependencies(fname):
 if __name__ == '__main__':
     setup(
         name='turkey_eq',
-        version='1.0',
+        version='1.2',
         description='Package for practical work on Turkey_EQ.',
         license='MIT',
         url='https://github.com/EkaterinaKugot/Turkey_EQ_notebook.git',
